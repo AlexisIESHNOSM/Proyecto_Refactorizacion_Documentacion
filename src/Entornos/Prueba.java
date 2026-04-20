5d1d939887f2d8dba27f41bca6a59aaa5b1394eb
Alexis <xadomcas142@ieshnosmachado.org>
2026-04-20 14:37:54 +0200

    Añadido método sumar

--- src/Entornos/Prueba.java
+++ src/Entornos/Prueba.java
@@ -5,8 +5,4 @@ public class Prueba {
     public void saludo() {
         System.out.println("Hola, iniciando control de versiones");
     }
-    
-    public int sumar(int a, int b) {
-        return a + b;
-    }
 }
