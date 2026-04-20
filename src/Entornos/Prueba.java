6b598b20643d46028e29cdd795b3105511c9515c
Alexis <xadomcas142@ieshnosmachado.org>
2026-04-20 21:56:56 +0200

    Pruebas finalizadas

--- src/Entornos/Prueba.java
+++ src/Entornos/Prueba.java
@@ -1,8 +1,12 @@
 package Entornos;
 
 public class Prueba {
-	// Versión inicial para el primer commit
-    public void saludo() {
-        System.out.println("Hola, iniciando control de versiones");
-    }
+	public void saludo() {
+		System.out.println("Hola, iniciando control de versiones");
+	}
+	
+	public int sumar(int a, int b) {
+		return a + b;
+	}
+	/*Este comentario finaliza el control de versiones*/
 }
